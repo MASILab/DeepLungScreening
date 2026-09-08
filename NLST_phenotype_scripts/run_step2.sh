5f9dfd2fdfc1f3455f0c75ba90237efc2982f193
AravindRK99 <aravind1999@gmail.com>
2026-09-07 23:09:46 -0500

    Step 2 nodule detection

--- NLST_phenotype_scripts/run_step2.sh
+++ NLST_phenotype_scripts/run_step2.sh
@@ -2,18 +2,23 @@
 ROOT="/valiant02/masi/krishar1/NLST_phenotype_journal_extension_from_SPIE/paired_NLST/DeepLungScreening"
 ORI_ROOT=${ROOT}/nifti
 PREP_ROOT=${ROOT}/prep
+BBOX_ROOT=${ROOT}/bbox
+
 
 mkdir -p ${ORI_ROOT}
 mkdir -p ${PREP_ROOT}
-echo "Starting step 1 preprocessing..."
+mkdir -p ${BBOX_ROOT}
+
+echo "Starting step 2 nodule detection..."
 echo "Using original root: ${ORI_ROOT}"
 echo "Using prep root: ${PREP_ROOT}"
+echo "Using bbox root: ${BBOX_ROOT}"
 echo "Using log directory: ${ROOT}"
 
 
-python /home-local/krishar1/DeepLungScreening/1_preprocess/step1_main.py \
+python /home-local/krishar1/DeepLungScreening/2_nodule_detection/step2_main.py \
  --sess_csv /home-local/krishar1/NLST_nodule_phenotype_extension/NLST_phenotype_journal_extension/spreadsheets_analysis/cancer_nlst_for_dls_preparation_9_7_26.csv \
  --prep_root ${PREP_ROOT} \
- --ori_root ${ORI_ROOT} \
- --log_dir ${ROOT} \
- --n_jobs 30
+ --bbox_root ${BBOX_ROOT} \
+ --gpu 1 \
+
