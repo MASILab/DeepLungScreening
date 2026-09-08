82956b231a662882926069bafbbd75f29a16fd70
AravindRK99 <aravind1999@gmail.com>
2026-09-07 22:09:49 -0500

    Fixed argument bug

--- NLST_phenotype_scripts/run_step1.sh
+++ NLST_phenotype_scripts/run_step1.sh
@@ -4,7 +4,8 @@ ROOT="/valiant02/masi/krishar1/NLST_phenotype_journal_extension_from_SPIE/paired
 ORI_ROOT=${ROOT}/nifti
 PREP_ROOT=${ROOT}/prep
 
-python /home-local/krishar1/DeepLungScreening/1_preprocess/step1_main.py /home-local/krishar1/NLST_nodule_phenotype_extension/NLST_phenotype_journal_extension/spreadsheets_analysis/cancer_nlst_for_dls_preparation_9_7_26.csv \
+python /home-local/krishar1/DeepLungScreening/1_preprocess/step1_main.py \
+ --sess_csv /home-local/krishar1/NLST_nodule_phenotype_extension/NLST_phenotype_journal_extension/spreadsheets_analysis/cancer_nlst_for_dls_preparation_9_7_26.csv \
  --prep_root ${PREP_ROOT} \
  --ori_root ${ORI_ROOT} \
  --log_dir ${ROOT} \
