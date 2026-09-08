9743174871d4eebdd26a7a63020af5c07d0d97c6
AravindRK99 <aravind1999@gmail.com>
2026-09-07 22:03:16 -0500

    Reflected logging in the shell script

--- NLST_phenotype_scripts/run_step1.sh
+++ NLST_phenotype_scripts/run_step1.sh
@@ -7,4 +7,5 @@ PREP_ROOT=${ROOT}/prep
 python /home-local/krishar1/DeepLungScreening/1_preprocess/step1_main.py /home-local/krishar1/NLST_nodule_phenotype_extension/NLST_phenotype_journal_extension/spreadsheets_analysis/cancer_nlst_for_dls_preparation_9_7_26.csv \
  --prep_root ${PREP_ROOT} \
  --ori_root ${ORI_ROOT} \
+ --log_dir ${ROOT} \
  --n_jobs 30
