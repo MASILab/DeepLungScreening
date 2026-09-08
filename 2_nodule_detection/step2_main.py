e9a9bd2f95f5d895a0e07c2aca0d890c9a2db5c8
AravindRK99 <aravind1999@gmail.com>
2026-09-07 23:49:11 -0500

    Weights fix to run on server

--- 2_nodule_detection/step2_main.py
+++ 2_nodule_detection/step2_main.py
@@ -37,8 +37,8 @@ args = parser.parse_args()
 config['datadir'] = args.prep_root
 
 # sess_splits = pd.read_csv(args.sess_csv)['pid'].tolist()
-sess_splits = pd.read_csv(args.sess_csv, dtype={'pid':str})
-sess_splits = sorted(sess_splits[~sess_splits['pid'].isnull()]['pid'].tolist())
+sess_splits = pd.read_csv(args.sess_csv, dtype={'id':str})
+sess_splits = sorted(sess_splits[~sess_splits['id'].isnull()]['id'].tolist())
 
 #Trial image for MCL adenocarcinoma with nodule size = 0 mm
 # df = pd.read_csv(args.sess_csv)
@@ -48,7 +48,7 @@ config['testsplit'] = sess_splits
 
 nodmodel = import_module('net_detector')
 config1, nod_net, loss, get_pbb = nodmodel.get_model()
-checkpoint = torch.load('/home-local/krishar1/DeepLungScreening/2_nodule_detection/detector.ckpt')
+checkpoint = torch.load('/home-local/krishar1/DeepLungScreening/2_nodule_detection/detector.ckpt', weights_only=False)
 nod_net.load_state_dict(checkpoint['state_dict'])
 
 device = torch.device(f"cuda:{args.gpu}")
