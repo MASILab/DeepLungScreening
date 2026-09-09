2d2fb8cb3f6133bfce04a11cf5d70bfb7289c8a4
AravindRK99 <aravind1999@gmail.com>
2026-09-08 19:11:35 -0500

    Step 3 fix

--- NLST_phenotype_scripts/run_step3.sh
+++ NLST_phenotype_scripts/run_step3.sh
@@ -26,3 +26,4 @@ python /home-local/krishar1/DeepLungScreening/3_feature_extraction/step3_main.py
     --prep_root ${PREP_ROOT} \
     --bbox_root ${BBOX_ROOT} \
     --feat64 ${FEAT64} \
+    --feat128 ${FEAT128} 
